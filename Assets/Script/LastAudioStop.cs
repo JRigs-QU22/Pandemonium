8531e44e5e6c7fa48f67256e97f57cd23c930058
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2022-05-10 18:35:00 -0400

    fix audio overlapped in tut

--- Assets/Script/LastAudioStop.cs
+++ Assets/Script/LastAudioStop.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class LastAudioStop : MonoBehaviour
 {
     public AudioSource LastAudio;
+    public AudioSource currentAudio;
+    [SerializeField]
+    private EnemyBase[] enemies;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = FindObjectsOfType<EnemyBase>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentAudio.isPlaying)
+        {
+            stopAllAudio();
+        }
     }
     void OnTriggerEnter(Collider other)
     {
@@ -23,9 +29,25 @@ public class LastAudioStop : MonoBehaviour
         if (other.tag == "Player")
         {
             LastAudio.Stop();
-
+        }
+    }
+    void stopAllAudio()
+    {
+        try
+        {
+            foreach (EnemyBase enemy in enemies)
+            {
+                if (enemy.KillAudio.isPlaying) enemy.KillAudio.Stop();
+                if (enemy.HSAudio.isPlaying) enemy.HSAudio.Stop();
+                if (enemy.PartialCombo.isPlaying) enemy.PartialCombo.Stop();
+                if (enemy.FullCombo.isPlaying) enemy.FullCombo.Stop();
+            }
+        }
+        catch
+        {
 
         }
+            
     }
 
 }
